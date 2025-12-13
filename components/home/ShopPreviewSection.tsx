1ad00873a9e5c722badde3f5920846ec8a2300ca
John <john.msme1@gmail.com>
2025-12-13 05:13:28 +0100

    ...

--- components/home/ShopPreviewSection.tsx
+++ components/home/ShopPreviewSection.tsx
@@ -88,7 +88,7 @@ const ShopPreviewSection = () => {
                     </motion.div>
 
                     {/* Products Grid */}
-                    <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6 mb-10">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6 mb-10">
                         {products.map((product, index) => (
                             <motion.div
                                 key={product.id}
